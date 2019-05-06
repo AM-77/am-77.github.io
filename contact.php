db47aadae17fefcd24bd760f86e579014415104d
AM77 <amine.griche77@gmail.com>
2019-05-06 02:58:38 +0200

    adding the website to git

---
+++ contact.php
@@ -0,0 +1,11 @@
+<?php
+$to      = 'amine.griche77@gmail.com';
+$subject = $_POST['name'];
+$message = $_POST['message'];
+$headers = 'From: '. $_POST['email'] . "\r\n" .
+    'Reply-To: ' . $to . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+header("Location: http://medaminegriche.tk/");
+?> 
