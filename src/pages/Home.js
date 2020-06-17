63cc57391d49a92f93098270f13a988f16ee422b
AM-77 <amine.griche77@gmail.com>
2020-06-17 22:42:30 +0200

    add home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,57 +1,72 @@
 import React, { Component } from 'react'
 import { gsap, Power3 } from 'gsap'
-
 import meinFoto from '../assets/images/mein-foto.jpg'
+
+import { NavLink } from 'react-router-dom'
+
 
 export default class Home extends Component {
 
-    // componentDidMount(){
-    //     const image_01_container = this.refs.image_01_container
-    //     const image_01 = this.refs.image_01
-    //     const image_02_container = this.refs.image_02_container
-    //     const image_02 = this.refs.image_02
-        
-    //     const title_lines = [this.refs.title_line_1, this.refs.title_line_2]
-    //     const description_lines = [this.refs.description_line_1, this.refs.description_line_2, this.refs.description_line_3]
+    componentDidMount(){
+        // head
+        const first_name = this.refs.first_name
+        const last_name = this.refs.last_name
+        const title = this.refs.title
+        const mein_foto = this.refs.mein_foto
+        const head = [first_name, last_name, title, mein_foto]
 
-    //     const the_eye = this.refs.the_eye
+        // about
+        const about_title = this.refs.about_title
+        const about_content1 = this.refs.about_content1
+        const about_content2 = this.refs.about_content2
+        const about = [about_title, about_content1, about_content2]
 
-    //     gsap.from(image_01_container, 1.8, { delay: 1, y: 1000,ease: Power3.easeOut})
-    //     gsap.from(image_01, 2, {delay: 1.6, scale: 2, ease: Power3.easeOut})
-    //     gsap.from(image_02_container, 1.9, { delay: 1.2, y: 1000, ease: Power3.easeOut })
-    //     gsap.from(image_02, 2.8, {delay: 1.6, scale: 2, ease: Power3.easeOut})
-    //     gsap.from(title_lines, 1.6, { delay: 1.6, ease: Power3.easeOut, y: 50, opacity: 0, stagger:{ amount: 0.2 } })
-    //     gsap.from(description_lines, 1.6, { delay: 1.8, ease: Power3.easeOut, y: 30, opacity: 0, stagger:{ amount: 0.2 } })
-    //     gsap.from(the_eye, 1.8, { delay: 1.8, rotate: -560, opacity: 0, scale: 0.75, ease: Power3.easeOut })
-    // }
+        // contact
+        const contact_title = this.refs.contact_title
+        const contact_content = this.refs.contact_content
+        const contact = [contact_title, contact_content]
+
+        gsap.from([...head, ...about, ...contact], 1.25, { delay: 0.25, ease: Power3.easeOut, opacity: 0, y: 50, stagger:{ amount: 0.75 } })
+
+        // contact links
+        const links = this.refs.contact_links.children
+        gsap.from(links, .6, { delay: 1.25, ease: Power3.easeOut, y: 50, opacity: 0, stagger:{ amount: 0.5 } })
+    }
 
     render() {
         return (
             <div className='home-container'>
-                <div className='head'>
-                    <div className='name'>
-                        <h1><span className='first'>Mohamed Amine</span> <span className='last'>Griche</span></h1>
-                        <div className='mein-foto'>
-                            <img src={meinFoto} alt='mien foto' />
+                <div className='center'>
+                    <div className='head'>
+                        <div className='name'>
+                            <div>
+                                <h1><span ref='first_name' className='first'>Mohamed Amine</span> <span ref='last_name' className='last'>Griche</span></h1>
+                                <h4><span ref='title' className='title'>Software Engineer & a JS Lover <span className='heart-emoji' aria-label='emoji' role='img'>💛</span>.</span></h4>
+                            </div>
+                            <div ref='mein_foto' className='mein-foto'>
+                                <img src={meinFoto} alt='mien foto' />
+                            </div>
                         </div>
                     </div>
-                    <h4 className='grade'><span>Software Engineer & JS Lover <span aria-label='emoji' role='img'>💛</span>.</span></h4>
-                </div>
 
-                <div className='About'>
-                    <h2 className='title'>About</h2>
-                    <p className='content'><span>Hello, my name is Amine <span aria-label='emoji' role='img'>👋</span></span></p>
-                    <p className='content'><span>I am a software engineer from Algeria.</span></p>
-                </div>
+                    <div className='about'>
+                        <h2 className='title'><span ref='about_title'>About</span></h2>
+                        <p className='content'><span ref='about_content1'>Hello, my name is Amine <span className='waving-emoji' aria-label='emoji' role='img'>👋</span></span></p>
+                        <p className='content'><span ref='about_content2'>I am a software engineer from Algeria.</span></p>
+                        <NavLink to='/work'>Here are some of my works.</NavLink>
+                    </div>
 
-                <div className='contact'>
-                    <h2 className='title'>Contact</h2>
-                    <p className='content'><span>Here are some places on the web where you can find me:</span></p>
-                    <div className='links'>
-                        <a href='https://github.com/am-77' title='github'>GitHub</a>
-                        <a href='https://www.linkedin.com/in/mohamed-amine-griche' title='linkedin'>LinkedIn</a>
-                        <a href='https://codepen.io/am-77' title='codepen'>CodePen</a>
-                        <a href='mailto:amine.griche77@gmail.com' title='email'>Email</a>
+                    <div className='contact'>
+                        <h2 className='title'><span ref='contact_title'>Reach Me</span></h2>
+                        <p className='content'><span ref='contact_content'>Here are some places on the web where you can find me:</span></p>
+                        <div ref='contact_links' className='links'>
+                            <a ref='twitter' href='https://github.com/am-77' title='am-77'>GitHub</a>
+                            <a href='https://www.linkedin.com/in/mohamed-amine-griche' title='mohamed-amine-griche'>LinkedIn</a>
+                            <a href='https://codepen.io/am-77' title='am-77'>CodePen</a>
+                            <a href='https://twitter.com/__AM77__' title='__AM77__'>Twitter</a>
+                            <a href='mailto:amine.griche77@gmail.com' title='amine.griche77@gmail.com'>Email</a>
+                            <a href='../assets/pdf/cv.pdf' title='email'>Resume</a>
+                        </div>
                     </div>
                 </div>
             </div>
