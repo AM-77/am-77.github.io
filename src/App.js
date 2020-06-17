be6a2c2ec0d29b5f8411a1b9f6463a74e1e03df5
AM-77 <amine.griche77@gmail.com>
2020-06-17 22:43:35 +0200

    dark theme toggler

--- src/App.js
+++ src/App.js
@@ -7,26 +7,27 @@ import Work from './pages/Work'
 
 export default class App extends Component {
 
-    onEnter = node => { gsap.from(node, 1.2, { delay: 1, opacity: 0 }) }
-    onExit = node => { gsap.to(node, 1.2, { opacity: 0 }) }
+    onEnter = node => { gsap.from(node, 1, { delay: 0.7, opacity: 0 })  }
+    onExit = node => { gsap.to(node, 1, { opacity: 0 }) }
 
-    componentDidMount() { gsap.to(this.refs.app, 0, {css: { visibility: 'visible' }}) }
+    componentDidMount() { 
+        if ( new Date().getHours() > 18 ) document.body.classList.add('dark')
+        gsap.to(this.refs.app, 0, {css: { visibility: 'visible' }}) 
+    }
 
     render() {
         return (
             <div className='app-container' ref='app'>
-                <div className='content-container'>
-                    <Route render={({location})=>(
-                        <TransitionGroup>
-                            <CSSTransition key={location.key} timeout={1200} onEnter={this.onEnter} onExit={this.onExit} >
-                                <Switch location={location}>
-                                    <Route path='/' exact={true}><Home /></Route>
-                                    <Route path='/work'><Work /></Route>
-                                </Switch>
-                            </CSSTransition>
-                        </TransitionGroup>
-                    )} />
-                </div>
+                <Route render={({ location })=>(
+                    <TransitionGroup>
+                        <CSSTransition key={location.key} timeout={1200} onEnter={this.onEnter} onExit={this.onExit} >
+                            <Switch location={location}>
+                                <Route path='/' exact={true}><Home /></Route>
+                                <Route path='/work'><Work /></Route>
+                            </Switch>
+                        </CSSTransition>
+                    </TransitionGroup>
+                )} />
             </div>
         )
     }
