6efe3c8d18ac94de13d1abcc3453a390134372ec
AM-77 <amine.griche77@gmail.com>
2020-06-18 05:21:37 +0200

    add preloader component

---
+++ src/components/Preloader.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+import logo from '../assets/images/logo.svg'
+
+export default class Preloader extends Component {
+
+  componentDidMount () {
+    const preloader = this.refs.preloader
+    const logo = this.refs.logo
+
+    this.props.tl
+      .from(logo, .75, { delay: 1, opacity: 0, y: 77 }) 
+      .to(logo, .75, { delay: .5, opacity: 0, y: -77 }) 
+      .to(preloader, 0, { delay: 1, css: { display: 'none' }}) 
+      .play()
+  }
+
+  render() {
+    return (
+      <div ref='preloader' className='preloader'>
+        <div ref='logo' className='logo-container'>
+          <img src={logo} alt='AM-77 Logo'/>
+        </div>
+      </div>
+    )
+  }
+}
