e0c22a8f30eac786d5c4b6a097a85f107a5c04b0
AM-77 <amine.griche77@gmail.com>
2020-06-18 05:41:10 +0200

    production mode

---
+++ precache-manifest.c6727200a51399f82cd324b0fe21cdc6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5413f2837276652948518917d9f4e9a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c34882bcb1025bebefe8",
+    "url": "/static/css/main.a5d6652c.chunk.css"
+  },
+  {
+    "revision": "c1ef2b7f6dfec85296d0",
+    "url": "/static/js/2.0ef97aed.chunk.js"
+  },
+  {
+    "revision": "8102be1f828c972acc1d2b1142a3c43e",
+    "url": "/static/js/2.0ef97aed.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c34882bcb1025bebefe8",
+    "url": "/static/js/main.153c5fee.chunk.js"
+  },
+  {
+    "revision": "79ab261fc46a48e3b06b",
+    "url": "/static/js/runtime-main.59326069.js"
+  },
+  {
+    "revision": "2b1372dbd65796d41fe05791161b0a3c",
+    "url": "/static/media/Cool-For-The-Momenet.2b1372db.ttf"
+  },
+  {
+    "revision": "0f4a76c9a0a22cd3b1e6f94712862cec",
+    "url": "/static/media/logo.0f4a76c9.svg"
+  },
+  {
+    "revision": "f63bed32390ea28270f3c609dffa8003",
+    "url": "/static/media/mein-foto.f63bed32.jpg"
+  }
+]);
