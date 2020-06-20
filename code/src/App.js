e9d0a152d8a7ebe6f498d748b7d5f0795e223f21
AM-77 <amine.griche77@protonmail.ch>
2020-06-20 21:31:42 +0200

    add <Preload />

--- code/src/App.js
+++ code/src/App.js
@@ -4,12 +4,20 @@ import { gsap, TimelineLite } from 'gsap'
 import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import Home from './pages/Home'
 import Work from './pages/Work'
+import Loader from './components/Loader'
+const Preload = require('react-preload').Preload;
 
 export default class App extends Component {
 
     constructor(props) {
         super(props)
         this.tl = new TimelineLite()
+        this.loadingIndicator = (<Loader />)
+        this.images = [
+            './assets/fonts/Cool-For-The-Momenet.ttf',
+            './assets/images/logo.svg',
+            './assets/images/mein-foto.png'
+        ]
     }
 
     onEnter = node => { gsap.from(node, 1, { delay: 1, opacity: 0 }) }
@@ -22,18 +30,23 @@ export default class App extends Component {
 
     render() {
         return (
-            <div className='app-container' ref='app'>
-                <Route render={({ location })=>(
-                    <TransitionGroup>
-                        <CSSTransition key={location.key} timeout={1000} onEnter={this.onEnter} onExit={this.onExit} >
-                            <Switch location={location}>
-                                <Route path='/' exact={true}><Home tl={this.tl} /></Route>
-                                <Route path='/work'><Work tl={this.tl} /></Route>
-                            </Switch>
-                        </CSSTransition>
-                    </TransitionGroup>
-                )} />
-            </div>
+            <Preload
+                loadingIndicator={this.loadingIndicator}
+                images={this.images}>
+                <div className='app-container' ref='app'>
+                    <Route render={({ location })=>(
+                        <TransitionGroup>
+                            <CSSTransition key={location.key} timeout={1000} onEnter={this.onEnter} onExit={this.onExit} >
+                                <Switch location={location}>
+                                    <Route path='/' exact={true}><Home tl={this.tl} /></Route>
+                                    <Route path='/work'><Work tl={this.tl} /></Route>
+                                </Switch>
+                            </CSSTransition>
+                        </TransitionGroup>
+                    )} />
+                </div>
+            </Preload>
+            
         )
     }
 }
