e093399e295f56fc1a3681d6e254d416aa7af97d
AM-77 <amine.griche77@protonmail.ch>
2020-07-20 04:13:50 +0200

    create <Cursor />

---
+++ code/src/components/Cursor.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import gsap from 'gsap'
+
+export default class Cursor extends Component {
+
+  constructor(props) {
+    super(props);
+    this.bigBall = React.createRef();
+    this.smallBall = React.createRef();
+  }
+
+  // Listeners
+  componentDidMount() {
+    document.body.addEventListener("mousemove", this.onMouseMove);
+    const links = document.querySelectorAll("a")
+    for (let i = 0; i < links.length; i++) {
+      links[i].addEventListener("mouseenter", this.onMouseHover);
+      links[i].addEventListener("mouseleave", this.onMouseHoverOut);
+    }
+  }
+
+  // Move the cursor
+  onMouseMove = (e) => {
+    gsap.to(this.bigBall.current, 0.4, { x: e.pageX - 15, y: e.pageY - 15 })
+    gsap.to(this.smallBall.current, 0.1, { x: e.pageX - 5, y: e.pageY - 7 })
+  }
+
+  // Hover an element
+  onMouseHover = () => gsap.to(this.bigBall.current, 0.3, { scale: 4 })
+  onMouseHoverOut = () => gsap.to(this.bigBall.current, 0.3, { scale: 1 })
+
+  render() {
+    return (
+      <div className="cursor">
+        <div ref={this.bigBall} className="cursor-ball big">
+          <svg height="30" width="30">
+            <circle cx="15" cy="15" r="12"></circle>
+          </svg>
+        </div>
+        <div ref={this.smallBall} className="cursor-ball small">
+          <svg height="10" width="10">
+            <circle cx="5" cy="5" r="4"></circle>
+          </svg>
+        </div>
+      </div>
+    )
+  }
+}
