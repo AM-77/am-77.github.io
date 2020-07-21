54c7d8b361d00eb4b14166c15c6e9c7ff407502d
AM-77 <amine.griche77@protonmail.ch>
2020-07-21 21:42:54 +0200

    add ReactFullpage & Cursor

--- code/src/App.js
+++ code/src/App.js
@@ -3,10 +3,12 @@ import { Switch, Route } from 'react-router-dom'
 import { gsap, TimelineLite } from 'gsap'
 import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import Home from './pages/Home'
-import Work from './pages/Work'
+import Portfolio from './pages/Portfolio'
 import Loader from './components/Loader'
-const Preload = require('react-preload').Preload;
+import Cursor from './components/Cursor'
+import ReactFullpage from '@fullpage/react-fullpage'
 
+const Preload = require('react-preload').Preload
 export default class App extends Component {
 
     constructor(props) {
@@ -18,35 +20,65 @@ export default class App extends Component {
             './assets/images/logo.svg',
             './assets/images/mein-foto.png'
         ]
+        this.state = {
+            links: []
+        }
     }
 
-    onEnter = node => { gsap.from(node, 1, { delay: 1, opacity: 0 }) }
+    onEnter = node => { 
+        gsap.from(node, 1, { delay: 1, opacity: 0 }) 
+        const links = document.querySelectorAll("a")
+        this.setState({ links })
+    }
+
     onExit = node => { gsap.to(node, 1, { opacity: 0 }) }
 
     componentDidMount() { 
-        if ( new Date().getHours() < 7 || new Date().getHours() > 18 ) document.body.classList.add('dark')
+        document.body.classList.add('dark')
         gsap.to(this.refs.app, 0, {css: { visibility: 'visible' }}) 
+
+        setTimeout(() => { 
+            const links = document.querySelectorAll("a")
+            this.setState({ links })
+        }, 1200)
     }
 
     render() {
         return (
-            <Preload
-                loadingIndicator={this.loadingIndicator}
-                images={this.images}>
-                <div className='app-container' ref='app'>
-                    <Route render={({ location })=>(
-                        <TransitionGroup>
-                            <CSSTransition key={location.key} timeout={1000} onEnter={this.onEnter} onExit={this.onExit} >
-                                <Switch location={location}>
-                                    <Route path='/' exact={true}><Home tl={this.tl} /></Route>
-                                    <Route path='/work'><Work tl={this.tl} /></Route>
-                                </Switch>
-                            </CSSTransition>
-                        </TransitionGroup>
-                    )} />
-                </div>
-            </Preload>
-            
+           <div ref='app'>   
+                <Cursor links={this.state.links} />
+                <Preload
+                    loadingIndicator={this.loadingIndicator}
+                    images={this.images}>
+                    <div className='app-container'>
+                        <Route render={({ location })=>(
+                            <TransitionGroup>
+                                <CSSTransition 
+                                    key={location.key} 
+                                    timeout={1000} 
+                                    onEnter={this.onEnter} 
+                                    onExit={this.onExit} 
+                                    >
+                                    <Switch location={location}>
+                                        <ReactFullpage
+                                            licenseKey = {'LICENSE_KEY'}
+                                            scrollingSpeed = {1000}
+                                            render={({ state, fullpageApi }) => {
+                                                return (
+                                                    <ReactFullpage.Wrapper>
+                                                        <Route path='/' exact={true}><Home fullpageApi={fullpageApi} tl={this.tl} /></Route>
+                                                        <Route path='/portfolio'><Portfolio fullpageApi={fullpageApi} tl={this.tl} /></Route>
+                                                    </ReactFullpage.Wrapper>
+                                                )
+                                            }}
+                                        />              
+                                    </Switch>
+                                </CSSTransition>
+                            </TransitionGroup>
+                        )} />
+                    </div>
+                </Preload>
+            </div>
         )
     }
 }
